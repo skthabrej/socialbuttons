3241f5b1841f045b03d15fab7a2ca2bf49a64231
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-10 10:06:03 +0530

    userslist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import UserProfile from "./Components/profiles/profile"
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl:
+      'https://i.ibb.co/rQg1q5T/dennis-ritchie6.webp',
+    name: 'Dennis Ritchie',
+    role: 'Father of C programming language'
+  },
+  {
+    uniqueNo: 2,
+    imageUrl:
+      'https://i.ibb.co/ypQYfzN/origin.jpg',
+    name: 'Linus Torvalds',
+    role: 'Founder of Linux kernel Operating system'
+  },
+  {
+    uniqueNo: 3,
+    imageUrl:
+      'https://i.ibb.co/BKqqSSJ/316-Kj-DKB7y-L-UY200.jpg',
+    name: 'Bjarne Stroustrup',
+    role: 'Invented C++ language'
+  },
+  {
+    uniqueNo: 4,
+    imageUrl:
+      'https://i.ibb.co/b5QpLgq/JAG2001.jpg',
+    name: 'James Gosling',
+    role: 'Father of JAVA programming language'
+  }
+]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Pillars of Programming</h1>
+    <ul>
+      {userDetailsList.map((eachItem) => (
+        <UserProfile userDetails={eachItem} />
+      ))}
+    </ul>
+  </div>
+)
+export default App
